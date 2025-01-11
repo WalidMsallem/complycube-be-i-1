ad01d95bbe770e7c6c0de63c1f0345c1857215b4
Walid Msallem <walidmsallem@gmail.com>
2025-01-11 00:35:13 +0000

    include clientId in client-token endpoint

--- api/create-client-and-return-sdk-token.js
+++ api/create-client-and-return-sdk-token.js
@@ -1,16 +1,20 @@
 import { generateToken } from './generate-token.js'
 import { createClient } from './create-client.js'
 
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
   try {
     const createClientResponse = await createClient(req.body)
-    const generateTokenResponse = await generateToken({ clientId: createClientResponse.data.id })
+    const generateTokenResponse = await generateToken({
+      clientId: createClientResponse.data.id,
+    })
 
-    res.json(generateTokenResponse.data)
+    res.json({
+      token: generateTokenResponse.data.token,
+      clientId: createClientResponse.data.id,
+    })
   } catch (error) {
     console.error(
       'Error generating token:',
